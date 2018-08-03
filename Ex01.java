822dcf5bd529d4eb171405e1492ea56e0628dd89
Seran Johyun An <39766534+johyunan@users.noreply.github.com>
2018-08-03 09:11:57 +0900

    Add files via upload

---
+++ Ex01.java
@@ -0,0 +1,17 @@
+/*
+ * 증감 연산자 예제
+ */
+public class Ex01 {
+
+	public static void main(String[] args) {
+		
+		int i = 50;
+		int j;
+		
+		j=i++;
+		//j = i; i=i+1;
+		System.out.println("i = "+i+" j = "+j);
+		
+	}
+
+}
